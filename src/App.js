c2f034c2283f9c5e14101b3c72da3c24b9701669
sany <sunnyjuice94@gmail.com>
2022-04-14 21:32:08 +0900

    body start

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+// import imgA from '%PUBLIC_URL%/visual_00.jpg';
 
 function App() {
   return (
-    
+    <div id="wrap">
+      <header id="header">
+
+      </header>
+      <div id="container">
+        <div id="main">
+          <p class="img"><img src="/img/visual_00.jpg"/></p>
+          <p class="text"></p>
+        </div>
+      </div>
+      <footer id="footer">
+
+      </footer>
+    </div>
   );
 }
 
